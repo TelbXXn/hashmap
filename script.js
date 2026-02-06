7de8ef565379750bb5f52bca15c8e24a3bf64a8c
TelbXXn <106255232+TelbXXn@users.noreply.github.com>
2026-02-06 14:58:23 +0100

    Rework of set() and creation of get()

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ class HashMap {
   }
 
   hash(key) {
-    if (typeof(key) !== 'string') return console.log('Key must be a string');
+    if (typeof(key) !== 'string') throw new Error('Input needs to be a string!');
     let hashCode = 0;
 
     const primeNumber = 31;
@@ -23,13 +23,52 @@ class HashMap {
     let hashCode = this.hash(key);
 
     if (!this.buckets[hashCode]) {
-      this.buckets[hashCode] = ({[key]: value});
-    }
-    else if (this.buckets[hashCode]) 
-
-    else if (this.buckets[hashCode].hasOwnProperty(key) === true) {
-      this.buckets[hashCode][key] = value;
-      return console.log("Key value already exists... updating to new value");
+        this.buckets[hashCode] = [];
+        this.buckets[hashCode].push([key, value]);
+    } else {
+        let bucket = this.buckets[hashCode];
+        for (let b of bucket) {
+            if (b[0] === key) {
+                b[1] = value;
+                return console.log(`Key is already found: Updating value...`);
+            }
+        }             
+        this.buckets[hashCode].push([key, value]);
+        return console.log(`adding new entry`);
     }
   }
+
+  get(key) {
+    let hashCode = this.hash(key);
+    if (!this.buckets[hashCode]) return;
+    let bucket = this.buckets[hashCode];
+
+    for (let entry of bucket) {
+      if (entry[0] === key) return entry[1];
+    }
+  }
+
 }
+
+
+//   set(key, value) {
+//     let hashCode = this.hash(key);
+
+//     if (!this.buckets[hashCode]) {
+//       this.buckets[hashCode] = [{[key]: value}];
+//     } 
+//     else if (this.buckets[hashCode]) {
+//       let bucket = this.buckets[hashCode];
+//         for (let b of bucket) {
+//             if (b.hasOwnProperty(key) === true) {
+//                 b[key] = value;
+//                 return console.log("Key value already exists... updating to new value");
+//             }
+//         }
+//         this.buckets[hashCode].push({[key]: value});
+//         return console.log('Pushed a new object');
+    
+//       }
+
+//   }
+// }
